93dedccbc97f7a4ee37f48f084c6152e5cc01d37
bill <qthang1910@gmail.com>
2020-05-04 21:23:13 +0700

    week 9 lecturer

---
+++ a2/movieweb.js
@@ -0,0 +1,9 @@
+var moviePanelAM = document.getElementById("moviePanelAM");
+
+// moviePanelAM.addEventListener("click", function(){moviePanelAM.style.background = "white"});
+
+if (moviePanelAM){
+    moviePanelAM.addEventListener('click', function(e) { console.log(this);});
+}
+
+// moviePanelAM.addEventListener('click', function(e) { console.log(this);});
